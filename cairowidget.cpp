b43292bee43b739a5047fc07bb37812193c3899c
Janez Zemva <janezz55@gmail.com>
2020-05-09 12:55:54 +0200

    some fixes

--- cairowidget.cpp
+++ cairowidget.cpp
@@ -109,8 +109,8 @@ void CairoWidget::draw()
       auto const wh(h());
 
       cairo_rectangle(cr, 0, 0, ww, wh);
-      cairo_clip(cr);
       cairo_fill(cr);
+      cairo_clip(cr);
 
       //
       cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
