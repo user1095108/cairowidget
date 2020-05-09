88c35398c1fe220e029c9ab2b356764146835ee4
Janez Zemva <janezz55@gmail.com>
2020-05-09 12:27:40 +0200

    some fixes

--- example.cpp
+++ example.cpp
@@ -15,6 +15,34 @@ public:
   {
     cairo_scale(cr, w, h);
 
+    int i, j;
+    cairo_pattern_t *radpat, *linpat;
+
+    radpat = cairo_pattern_create_radial(0.25, 0.25, 0.1,  0.5, 0.5, 0.5);
+    cairo_pattern_add_color_stop_rgb(radpat, 0,  1.0, 0.8, 0.8);
+    cairo_pattern_add_color_stop_rgb(radpat, 1,  0.9, 0.0, 0.0);
+
+    for (i=1; i<10; i++)
+        for (j=1; j<10; j++)
+            cairo_rectangle(cr, i/10.0 - 0.04, j/10.0 - 0.04, 0.08, 0.08);
+    cairo_set_source(cr, radpat);
+    cairo_fill(cr);
+
+    linpat = cairo_pattern_create_linear(0.25, 0.35, 0.75, 0.65);
+    cairo_pattern_add_color_stop_rgba(linpat, 0.00,  1, 1, 1, 0);
+    cairo_pattern_add_color_stop_rgba(linpat, 0.25,  0, 1, 0, 0.5);
+    cairo_pattern_add_color_stop_rgba(linpat, 0.50,  1, 1, 1, 0);
+    cairo_pattern_add_color_stop_rgba(linpat, 0.75,  0, 0, 1, 0.5);
+    cairo_pattern_add_color_stop_rgba(linpat, 1.00,  1, 1, 1, 0);
+
+    cairo_rectangle(cr, 0.0, 0.0, 1, 1);
+    cairo_set_source(cr, linpat);
+    cairo_fill(cr);
+
+    cairo_pattern_destroy(linpat);
+    cairo_pattern_destroy(radpat);
+
+/*
     cairo_set_source_rgb(cr, 0, 0, 0);
     cairo_move_to(cr, 0, 0);
     cairo_line_to(cr, 1, 1);
@@ -34,6 +62,7 @@ public:
     cairo_rectangle(cr, .5, 0, .5, .5);
     cairo_set_source_rgba(cr, 0, 0, 1, .4);
     cairo_fill(cr);
+*/
   }
 };
 
