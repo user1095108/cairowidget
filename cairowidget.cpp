c094b871ae4db227558945d255fd57a4704e1a99
janezz55 <janezz55@gmail.com>
2020-11-25 14:48:50 +0100

    some fixes

--- cairowidget.cpp
+++ cairowidget.cpp
@@ -78,11 +78,7 @@ void CairoWidget::draw()
       {
         // ARGB (BGRA) to RGBx
         if constexpr (std::endian::little == std::endian::native)
-#if defined(__GNUC__)
-        *dst++ = __builtin_bswap32(*src++ << 8); // shift out A and swap
-#else
-        *dst++ = bswap<0, 1, 2, 3>(*src++ << 8);
-#endif
+          *dst++ = bswap<0, 1, 2, 3>(*src++ << 8);
         else
           *dst++ = *src++ << 8;
       }
