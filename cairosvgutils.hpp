3fe9334e5186c7407eb8ab6602c736321f80fbee
janezz55 <janezz55@gmail.com>
2020-11-26 10:13:58 +0100

    some fixes

--- cairosvgutils.hpp
+++ cairosvgutils.hpp
@@ -15,7 +15,21 @@ void draw_svg_image(cairo_t*, struct NSVGimage*,
 void draw_svg_image(Fl_Image*, struct NSVGimage*,
   double = 0, double = 0) noexcept;
 
-struct NSVGshape* find_svg_shape(struct NSVGimage*) noexcept;
+struct NSVGshape* find_svg_shape(struct NSVGimage*,
+  std::string_view const&) noexcept;
+
 void draw_svg_shape(cairo_t*, struct NSVGshape*) noexcept;
+void draw_svg_shape(cairo_t*, struct NSVGimage*,
+  std::string_view const&) noexcept;
+
+//////////////////////////////////////////////////////////////////////////////
+inline void draw_svg_shape(cairo_t* const cr, struct NSVGimage* const image, 
+  std::string_view const& name) noexcept
+{
+  if (auto const shape(find_svg_shape(image, name)); shape)
+  {
+    draw_svg_shape(cr, shape);
+  }
+}
 
 #endif // CAIRO_SVG_UTILS_HPP
