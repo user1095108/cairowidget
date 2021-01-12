073457ff4796e607fb5bff98153c95802de45015
Janez Žemva <janezz55@gmail.com>
2021-01-12 07:56:30 +0100

    some fixes

--- caironanosvg.cpp
+++ caironanosvg.cpp
@@ -310,7 +310,7 @@ void draw_svg_image(Fl_Image* const fli, struct NSVGimage* const image,
 
   // ARGB -> RGBA (selects bytes and places them MSB -> LSB)
   std::transform(std::execution::unseq,
-    src, src + std::uintptr_t(cairo_image_surface_get_stride(surf)) * h / 4,
+    src, src + std::size_t(cairo_image_surface_get_stride(surf)) * h / 4,
     reinterpret_cast<std::uint32_t*>(const_cast<char*>(fli->data()[0])),
     [](auto const a) noexcept { return shuffle<2, 1, 0, 3>(a); });
 
