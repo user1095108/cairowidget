9b8b38ceac5f9e110e9cb5b440142394a4504513
Janez Žemva <janezz55@gmail.com>
2021-05-14 01:23:05 +0200

    some fixes

---
+++ CairoWidget.cpp
@@ -0,0 +1,87 @@
+#include <memory>
+
+#include <QPainter>
+
+#include "CairoWidget.hpp"
+
+struct CairoWidget::S
+{
+  static inline std::size_t datasize_;
+  static inline std::unique_ptr<unsigned char[]> data_;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+CairoWidget::CairoWidget(QWidget* const p, Qt::WindowFlags const wf):
+  QWidget(p, wf),
+  df_{[](auto, auto, auto) noexcept {}},
+  if_{[](auto const cr, auto, auto) noexcept
+    {
+      cairo_set_line_width(cr, 1.);
+      cairo_translate(cr, .5, .5);
+    }
+  }
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////
+CairoWidget::~CairoWidget() noexcept
+{
+  cairo_destroy(cr_);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+void CairoWidget::paintEvent(QPaintEvent*)
+{
+  auto d(S::data_.get());
+
+  auto const w(this->width()), h(this->height());
+
+  {
+    auto cr(cr_);
+
+    if ((w != w_) || (h != h_) || (d != d_))
+    {
+      w_ = w; h_ = h;
+
+      //
+      auto const stride(stride_ =
+        cairo_format_stride_for_width(CAIRO_FORMAT_ARGB32, w));
+
+      {
+        auto const datasize(h * std::size_t(stride));
+
+        if (S::datasize_ < datasize)
+        {
+          S::data_.reset(d = new unsigned char[S::datasize_ = datasize]);
+        }
+      }
+
+      d_ = d;
+
+      //
+      cairo_destroy(cr);
+
+      auto const surf(cairo_image_surface_create_for_data(d,
+        CAIRO_FORMAT_RGB24, w, h, stride));
+      cr_ = cr = cairo_create(surf);
+      cairo_surface_destroy(surf);
+
+      //
+      if_(cr, w, h);
+    }
+
+    //
+    cairo_save(cr);
+
+    df_(cr, w, h);
+
+    cairo_restore(cr);
+  }
+
+  //
+  //cairo_surface_flush(surf);
+
+  QPainter painter(this);
+
+  painter.drawImage(0, 0, QImage(d, w, h, stride_, QImage::Format_ARGB32));
+}
---
+++ CairoWidget.hpp
@@ -0,0 +1,58 @@
+#ifndef CAIROWIDGET_HPP
+# define CAIROWIDGET_HPP
+# pragma once
+
+#include <QWidget>
+
+#include "cairo/cairo.h"
+
+#include <functional>
+
+class CairoWidget: public QWidget
+{
+  struct S;
+
+  cairo_t* cr_{};
+
+  int w_{}, h_;
+
+  unsigned char* d_;
+
+  unsigned stride_;
+
+  using draw_t = std::function<void(cairo_t*, int, int)>;
+
+  draw_t df_;
+  draw_t if_;
+
+public:
+  explicit CairoWidget(QWidget* = nullptr,
+    Qt::WindowFlags = Qt::WindowFlags());
+  ~CairoWidget() noexcept;
+
+  //
+  template <class U>
+  void init(U&& u) noexcept(noexcept(if_ = std::forward<U>(u)))
+  {
+    if_ = std::forward<U>(u);
+  }
+
+  auto& draw() const noexcept;
+
+  template <class U>
+  void draw(U&& u) noexcept(noexcept(df_ = std::forward<U>(u)))
+  {
+    df_ = std::forward<U>(u);
+  }
+
+private:
+  void paintEvent(QPaintEvent*) final;
+};
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto& CairoWidget::draw() const noexcept
+{
+  return df_;
+}
+
+#endif // CAIROWIDGET_HPP
