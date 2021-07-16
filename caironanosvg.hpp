a085b3a1b5373b80e753c803a32da52f7d9f1109
Janez Žemva <janezz55@gmail.com>
2021-07-16 08:29:17 +0200

    some fixes

--- caironanosvg.hpp
+++ caironanosvg.hpp
@@ -2,13 +2,13 @@
 # define CAIRO_SVG_UTILS_HPP
 # pragma once
 
-#include "cairo/cairo.h"
-
-#include "nanosvg.h"
-
 #include <cstring>
 
 #include <string_view>
+
+#include "cairo/cairo.h"
+
+#include "nanosvg/src/nanosvg.h"
 
 void draw_svg_image(cairo_t*, struct NSVGimage*,
   double = 0, double = 0, double = 0, double = 0) noexcept;
